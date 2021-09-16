269a7ce5f04d3a03b46e5c9db4e78669d5f343fa
joelvitorniino <joelprofessor13@gmail.com>
2021-09-16 18:02:11 -0300

    adding handle error

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ import wikipedia
 
 app = Flask(__name__)
 
-
-@app.route("/api/v1/search=<search>&lang=<lang>", methods=['GET'])
+@app.route("/api/v1/search=<search>&lang=<lang>", methods=["POST", "GET"])
 def home(search, lang):
     wikipedia.set_lang(f"{lang}")
-    result = wikipedia.summary(search, sentences=4)
-    if not result:
-        return jsonify({"data": "Não encontrado por favor tente novamente com outro nome parecido."})
+    try:
+       result = wikipedia.summary(search, sentences=4) 
+       return jsonify({"data": result})
+    except wikipedia.DisambiguationError:
+        return jsonify({"data": "Pesquisa não encontrada, por favor tente com outro nome parecido." })  
 
-    return jsonify({"data": result})
-
-    
 if __name__ == "__main__":
     app.run()
