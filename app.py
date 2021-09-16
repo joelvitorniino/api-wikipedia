ebb2ae2fdddfddee1d51fdeac2651d756faff491
joelvitorniino <joelprofessor13@gmail.com>
2021-09-16 17:37:11 -0300

    adding error

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ import wikipedia
 
 app = Flask(__name__)
 
+
 @app.route("/api/v1/search=<search>&lang=<lang>", methods=['GET'])
 def home(search, lang):
-        wikipedia.set_lang(f"{lang}")
-        result = wikipedia.summary(search, sentences = 4)
-        return jsonify({ "data": result })
+    wikipedia.set_lang(f"{lang}")
+    result = wikipedia.summary(search, sentences=4)
+    if not result:
+        return jsonify({"data": "Não encontrado por favor tente novamente com outro nome parecido."})
 
+    return jsonify({"data": result})
+
+    
 if __name__ == "__main__":
-    app.run()        
+    app.run()
