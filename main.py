17bdcd894d0ed70706e17e14d1d0af704f9c174b
joelvitorniino <joelprofessor13@gmail.com>
2021-09-16 08:37:01 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import wikipedia
+
+app = Flask(__name__)
+
+@app.route("/api/v1/search=<search>&lang=<lang>", methods=['GET'])
+def home(search, lang):
+        wikipedia.set_lang(f"{lang}")
+        result = wikipedia.summary(search, sentences = 4)
+        return f"{result}"
+
+if __name__ == "__main__":
+    app.run(debug=True)        
